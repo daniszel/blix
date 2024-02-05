398a1154456903f3165f1575c0d10d9c7d8ab895
Daniel Szeląg <dszelag@macro.local>
2024-02-05 06:02:34 +0100

    update validation

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Form, Input, Select, Space, Checkbox } from 'antd'
+import { Button, Form, Input, Select, Checkbox } from 'antd'
 import { CheckboxProps } from 'antd'
 
 const { Option } = Select
@@ -17,10 +17,6 @@ const DynamicForm: React.FC = () => {
 
   const onFinish = (values: string) => {
     console.log(values)
-  }
-
-  const onReset = () => {
-    form.resetFields()
   }
 
   const onChange: CheckboxProps['onChange'] = (e) => {
@@ -49,28 +45,31 @@ const DynamicForm: React.FC = () => {
       <Form.Item
         name='username'
         label='User Name'
+        rules={[{ type: 'email' }, { required: true }]}
       >
-        <Input
-          placeholder='name@example.com'
-          type='email'
-          required
-        />
+        <Input placeholder='name@example.com' />
       </Form.Item>
 
       <Form.Item
         name='password'
         label='Password'
+        required
       >
-        <Input
+        <Input.Password
           placeholder='Required'
           type='password'
-          required
         />
       </Form.Item>
 
       <Form.Item
         name='serverAddress'
         label='Server Address'
+        rules={[
+          {
+            pattern: new RegExp('^(?!$)(www\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$'),
+            message: 'wrong format',
+          },
+        ]}
       >
         <Input placeholder='example.com' />
       </Form.Item>
@@ -87,18 +86,33 @@ const DynamicForm: React.FC = () => {
               <Form.Item
                 name='serverPath'
                 label='Server Path'
+                rules={[
+                  {
+                    pattern: new RegExp('^[a-zA-Z0-9/]*$'),
+                    message: 'wrong format',
+                  },
+                ]}
               >
                 <Input placeholder='/calendars/users/' />
               </Form.Item>
               <Form.Item
                 name='port'
                 label='Port'
+                rules={[
+                  {
+                    pattern: new RegExp(
+                      '^(6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]\\d{4}|[1-9]\\d{0,3})$'
+                    ),
+                    message: 'wrong format',
+                  },
+                ]}
               >
                 <Input />
               </Form.Item>
               <Form.Item
                 name='encrypt'
                 label='Use SSL'
+                valuePropName='checked'
               >
                 <Checkbox
                   checked={checked}
@@ -110,21 +124,24 @@ const DynamicForm: React.FC = () => {
         }
       </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Space>
-          <Button
-            type='primary'
-            htmlType='submit'
-          >
-            Submit
-          </Button>
-          <Button
-            htmlType='button'
-            onClick={onReset}
-          >
-            Reset
-          </Button>
-        </Space>
+      <Form.Item
+        {...tailLayout}
+        style={{ display: 'flex', justifyContent: 'flex-end' }}
+        shouldUpdate
+      >
+        {({ getFieldsValue }) => {
+          const { username, password } = getFieldsValue()
+          const formIsComplete = !!username && !!password
+          return (
+            <Button
+              type='primary'
+              htmlType='submit'
+              disabled={!formIsComplete}
+            >
+              Submit
+            </Button>
+          )
+        }}
       </Form.Item>
     </Form>
   )
